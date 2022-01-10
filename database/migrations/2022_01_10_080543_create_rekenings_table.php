f08c97cc2137e519f8de43ef0b206ddcee218cb0
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-10 15:18:08 +0700

    add rekening migration

---
+++ database/migrations/2022_01_10_080543_create_rekenings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRekeningsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tm_rekening', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('kode_bank')->unique();
+            $table->string('no_rekening');
+            $table->string('atas_nama');
+            $table->timestamps();
+
+            $table->foreign('kode_bank')->references('kode_bank')->on('tm_bank');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tm_rekening');
+    }
+}
