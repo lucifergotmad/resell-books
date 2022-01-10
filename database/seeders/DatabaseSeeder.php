1d4e4736a7199e211a0d1a01866267c2d24d60a8
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-10 16:21:37 +0700

    fix database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Bank;
+use App\Models\Rekening;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory(3)->create();
+        Rekening::factory(3)->create();
     }
 }
