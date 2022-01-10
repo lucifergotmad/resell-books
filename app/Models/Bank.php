21415c1e3838be9f3045fe80793d47a6cfc06304
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-10 15:15:46 +0700

    fix bank model

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Bank extends Model
 {
     use HasFactory;
+
+    public function rekening() {
+        return $this->belongsTo(Rekening::class);
+    }
 }
