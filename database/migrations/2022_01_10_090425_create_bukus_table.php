63fb0451f7a59854fa4d1cca114cfb514ce794e1
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-10 16:22:56 +0700

    add migration for kategori & buku

---
+++ database/migrations/2022_01_10_090425_create_bukus_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tm_buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_buku')->unique();
+            $table->string('judul_buku')->unique();
+            $table->string('pengarang');
+            $table->double('berat');
+            $table->double('harga_jual');
+            $table->double('harga_sewa');
+            $table->foreignId('kode_kategori');
+            $table->string('penerbit');
+            $table->string('tahun_terbit');
+            $table->timestamps();
+
+            $table->foreign('kode_kategori')->references('kode_kategori')->on('tm_kategori');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tm_buku');
+    }
+}
