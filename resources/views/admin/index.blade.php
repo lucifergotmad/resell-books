c10336c3c67e14c4dd70f0217cca6d114437b7bd
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-12 10:38:18 +0700

    add favicon

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Tampilan Dashboard reSell App">
+    <meta name="author" content="lucifergotmad">
 
-    <title>re Sell - {{ $title }}</title>
+    <title>reSell - {{ $title }}</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
@@ -19,6 +19,10 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
 </head>
 
