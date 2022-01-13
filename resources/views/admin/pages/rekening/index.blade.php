b7667f8f80e99fc48e9f3017e6d0398f45da47b8
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-13 21:17:12 +0700

    fix rekening view

--- resources/views/admin/pages/rekening/index.blade.php
+++ resources/views/admin/pages/rekening/index.blade.php
@@ -44,7 +44,7 @@
                             </span>
                             <!--end::Svg Icon-->
                             <input type="text" data-kt-user-table-filter="search"
-                                class="form-control form-control-solid w-250px ps-14" placeholder="Cari data bank">
+                                class="form-control form-control-solid w-250px ps-14" placeholder="Cari data rekening">
                         </div>
                         <!--end::Search-->
                     </div>
@@ -53,6 +53,48 @@
                     <div class="card-toolbar">
                         <!--begin::Toolbar-->
                         <div class="d-flex justify-content-end" data-kt-user-table-toolbar="base">
+                            <button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                                <!--begin::Svg Icon | path: icons/duotune/general/gen031.svg-->
+                                <span class="svg-icon svg-icon-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                        <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="black"></path>
+                                    </svg>
+                                </span>
+                                <!--end::Svg Icon-->
+                            Filter</button>
+
+                            <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true">
+                                <!--begin::Header-->
+                                <div class="px-7 py-5">
+                                    <div class="fs-5 text-dark fw-bolder">Filter Options</div>
+                                </div>
+                                <!--end::Header-->
+                                <!--begin::Separator-->
+                                <div class="separator border-gray-200"></div>
+                                <!--end::Separator-->
+                                <!--begin::Content-->
+                                <div class="px-7 py-5" data-kt-user-table-filter="form">
+                                    <!--begin::Input group-->
+                                    <div class="mb-10">
+                                        <label class="form-label fs-6 fw-bold">Bank:</label>
+                                        <select class="form-select form-select-solid fw-bolder select2-hidden-accessible" data-kt-select2="true" data-placeholder="Select option" data-allow-clear="true" data-kt-user-table-filter="role" data-hide-search="true" data-select2-id="select2-data-10-oj63" tabindex="-1" aria-hidden="true">
+                                            <option data-select2-id="select2-data-12-ix32"></option>
+                                            <option value="Administrator">BCA - Bank Central Asia</option>
+                                            <option value="Administrator">BRI - Bank Republik Indonesia</option>
+                                            <option value="Administrator">BNI - Bank Negara Indonesia</option>
+                                        </select>
+                                    </div>
+                                    <!--end::Input group-->
+                                    <!--begin::Actions-->
+                                    <div class="d-flex justify-content-end">
+                                        <button type="reset" class="btn btn-light btn-active-light-primary fw-bold me-2 px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="reset">Reset</button>
+                                        <button type="submit" class="btn btn-primary fw-bold px-6" data-kt-menu-dismiss="true" data-kt-user-table-filter="filter">Apply</button>
+                                    </div>
+                                    <!--end::Actions-->
+                                </div>
+                                <!--end::Content-->
+                            </div>
+
                             <!--begin::Add user-->
                             <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                 data-bs-target="#kt_modal_add_user">
@@ -128,7 +170,20 @@
                                                 <!--begin::Input group-->
                                                 <div class="fv-row mb-7 fv-plugins-icon-container">
                                                     <!--begin::Label-->
-                                                    <label class="required fw-bold fs-6 mb-2">Nama Bank</label>
+                                                    <label class="required fw-bold fs-6 mb-2">No Rekening</label>
+                                                    <!--end::Label-->
+                                                    <!--begin::Input-->
+                                                    <input type="email" name="user_email"
+                                                        class="form-control form-control-solid mb-3 mb-lg-0"
+                                                        placeholder="Bank Republik Indonesia">
+                                                    <!--end::Input-->
+                                                    <div class="fv-plugins-message-container invalid-feedback"></div>
+                                                </div>
+                                                <!--end::Input group-->
+                                                 <!--begin::Input group-->
+                                                 <div class="fv-row mb-7 fv-plugins-icon-container">
+                                                    <!--begin::Label-->
+                                                    <label class="required fw-bold fs-6 mb-2">Atas Nama</label>
                                                     <!--end::Label-->
                                                     <!--begin::Input-->
                                                     <input type="email" name="user_email"
@@ -183,7 +238,8 @@
                                             No
                                         </th>
                                         <th>Kode Bank</th>
-                                        <th>Nama Bank</th>
+                                        <th>No Rekening</th>
+                                        <th>Atas Nama</th>
                                         <th></th>
                                             
                                     </tr>
@@ -197,7 +253,8 @@
                                             1
                                         </td>
                                         <td>BCA</td>
-                                        <td>Bank Central Asia</td>
+                                        <td>32056000723</td>
+                                        <td>Octyo Paswa Putra</td>
                                         <!--begin::Action=-->
                                         <td class="text-end">
                                             <a href="#" class="btn btn-light btn-active-light-primary btn-sm"
@@ -238,8 +295,8 @@
                                             2
                                         </td>
                                         <td>BRI</td>
-                                        <td>Bank Republik Indonesia</td>
-                                        <!--begin::Action=-->
+                                        <td>32056000321</td>
+                                        <td>Nur Amanah</td>                                        <!--begin::Action=-->
                                         <td class="text-end">
                                             <a href="#" class="btn btn-light btn-active-light-primary btn-sm"
                                                 data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">Actions
