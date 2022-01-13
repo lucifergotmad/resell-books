0acdf9c6bb2714361d32d70b391a24f947256874
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-13 13:08:08 +0700

    fix sidebar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -37,8 +37,8 @@
 										<span class="menu-section text-muted text-uppercase fs-8 ls-1">Master</span>
 									</div>
 								</div>
-								<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-									<span class="menu-link">
+								<div class="menu-item">
+									<a class="menu-link" href="../../demo1/dist/apps/calendar.html">
 										<span class="menu-icon">
 											<!--begin::Svg Icon | path: assets/media/icons/duotune/maps/map009.svg-->
                                             <span class="svg-icon svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
@@ -50,26 +50,62 @@
                                             <!--end::Svg Icon-->
 										</span>
 										<span class="menu-title">Alamat</span>
-										<span class="menu-arrow"></span>
-									</span>
-									<div class="menu-sub menu-sub-accordion menu-active-bg">
-										<div class="menu-item">
-											<a class="menu-link" href="../../demo1/dist/account/overview.html">
-												<span class="menu-bullet">
-													<span class="bullet bullet-dot"></span>
-												</span>
-												<span class="menu-title">Lihat Alamat</span>
-											</a>
-										</div>
-										<div class="menu-item">
-											<a class="menu-link" href="../../demo1/dist/account/settings.html">
-												<span class="menu-bullet">
-													<span class="bullet bullet-dot"></span>
-												</span>
-												<span class="menu-title">Tambah Alamat</span>
-											</a>
-										</div>
-									</div>
+									</a>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link" href="../../demo1/dist/apps/calendar.html">
+										<span class="menu-icon">
+										<!--begin::Svg Icon | path: assets/media/icons/duotune/finance/fin001.svg-->
+											<span class="svg-icon svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+												<path d="M20 19.725V18.725C20 18.125 19.6 17.725 19 17.725H5C4.4 17.725 4 18.125 4 18.725V19.725H3C2.4 19.725 2 20.125 2 20.725V21.725H22V20.725C22 20.125 21.6 19.725 21 19.725H20Z" fill="black"/>
+												<path opacity="0.3" d="M22 6.725V7.725C22 8.325 21.6 8.725 21 8.725H18C18.6 8.725 19 9.125 19 9.725C19 10.325 18.6 10.725 18 10.725V15.725C18.6 15.725 19 16.125 19 16.725V17.725H15V16.725C15 16.125 15.4 15.725 16 15.725V10.725C15.4 10.725 15 10.325 15 9.725C15 9.125 15.4 8.725 16 8.725H13C13.6 8.725 14 9.125 14 9.725C14 10.325 13.6 10.725 13 10.725V15.725C13.6 15.725 14 16.125 14 16.725V17.725H10V16.725C10 16.125 10.4 15.725 11 15.725V10.725C10.4 10.725 10 10.325 10 9.725C10 9.125 10.4 8.725 11 8.725H8C8.6 8.725 9 9.125 9 9.725C9 10.325 8.6 10.725 8 10.725V15.725C8.6 15.725 9 16.125 9 16.725V17.725H5V16.725C5 16.125 5.4 15.725 6 15.725V10.725C5.4 10.725 5 10.325 5 9.725C5 9.125 5.4 8.725 6 8.725H3C2.4 8.725 2 8.325 2 7.725V6.725L11 2.225C11.6 1.925 12.4 1.925 13.1 2.225L22 6.725ZM12 3.725C11.2 3.725 10.5 4.425 10.5 5.225C10.5 6.025 11.2 6.725 12 6.725C12.8 6.725 13.5 6.025 13.5 5.225C13.5 4.425 12.8 3.725 12 3.725Z" fill="black"/>
+												</svg>
+											</span>
+												<!--end::Svg Icon-->
+										</span>
+										<span class="menu-title">Bank</span>
+									</a>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link" href="../../demo1/dist/apps/calendar.html">
+										<span class="menu-icon">
+										<!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com005.svg-->
+											<span class="svg-icon svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+												<path d="M20 14H18V10H20C20.6 10 21 10.4 21 11V13C21 13.6 20.6 14 20 14ZM21 19V17C21 16.4 20.6 16 20 16H18V20H20C20.6 20 21 19.6 21 19ZM21 7V5C21 4.4 20.6 4 20 4H18V8H20C20.6 8 21 7.6 21 7Z" fill="black"/>
+												<path opacity="0.3" d="M17 22H3C2.4 22 2 21.6 2 21V3C2 2.4 2.4 2 3 2H17C17.6 2 18 2.4 18 3V21C18 21.6 17.6 22 17 22ZM10 7C8.9 7 8 7.9 8 9C8 10.1 8.9 11 10 11C11.1 11 12 10.1 12 9C12 7.9 11.1 7 10 7ZM13.3 16C14 16 14.5 15.3 14.3 14.7C13.7 13.2 12 12 10.1 12C8.10001 12 6.49999 13.1 5.89999 14.7C5.59999 15.3 6.19999 16 7.39999 16H13.3Z" fill="black"/>
+												</svg></span>
+												<!--end::Svg Icon-->
+										</span>
+										<span class="menu-title">Buku</span>
+									</a>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link" href="../../demo1/dist/apps/calendar.html">
+										<span class="menu-icon">
+										<!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com014.svg-->
+											<span class="svg-icon svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+												<path d="M16.0173 9H15.3945C14.2833 9 13.263 9.61425 12.7431 10.5963L12.154 11.7091C12.0645 11.8781 12.1072 12.0868 12.2559 12.2071L12.6402 12.5183C13.2631 13.0225 13.7556 13.6691 14.0764 14.4035L14.2321 14.7601C14.2957 14.9058 14.4396 15 14.5987 15H18.6747C19.7297 15 20.4057 13.8774 19.912 12.945L18.6686 10.5963C18.1487 9.61425 17.1285 9 16.0173 9Z" fill="black"/>
+												<rect opacity="0.3" x="14" y="4" width="4" height="4" rx="2" fill="black"/>
+												<path d="M4.65486 14.8559C5.40389 13.1224 7.11161 12 9 12C10.8884 12 12.5961 13.1224 13.3451 14.8559L14.793 18.2067C15.3636 19.5271 14.3955 21 12.9571 21H5.04292C3.60453 21 2.63644 19.5271 3.20698 18.2067L4.65486 14.8559Z" fill="black"/>
+												<rect opacity="0.3" x="6" y="5" width="6" height="6" rx="3" fill="black"/>
+												</svg></span>
+												<!--end::Svg Icon-->
+										</span>
+										<span class="menu-title">Member</span>
+									</a>
+								</div>
+								<div class="menu-item">
+									<a class="menu-link" href="../../demo1/dist/apps/calendar.html">
+										<span class="menu-icon">
+									<!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com013.svg-->
+											<span class="svg-icon svg-icon svg-icon-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+												<path d="M6.28548 15.0861C7.34369 13.1814 9.35142 12 11.5304 12H12.4696C14.6486 12 16.6563 13.1814 17.7145 15.0861L19.3493 18.0287C20.0899 19.3618 19.1259 21 17.601 21H6.39903C4.87406 21 3.91012 19.3618 4.65071 18.0287L6.28548 15.0861Z" fill="black"/>
+												<rect opacity="0.3" x="8" y="3" width="8" height="8" rx="4" fill="black"/>
+												</svg></span>
+												<!--end::Svg Icon-->
+										</span>
+										<span class="menu-title">Users</span>
+									</a>
 								</div>
 
                                 <div class="menu-item">
