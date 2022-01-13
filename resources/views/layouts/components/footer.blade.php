8d0c4d5c2e9dde937aec856974e3114162b4b127
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-13 09:53:59 +0700

    fix footer

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,9 +1,14 @@
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+<!--begin::Footer-->
+<div class="footer py-4 d-flex flex-lg-column" id="kt_footer">
+    <!--begin::Container-->
+    <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
+        <!--begin::Copyright-->
+        <div class="text-dark order-2 order-md-1">
+            <span class="text-muted fw-bold me-1">2022©</span>
+            <a href="/" target="_blank" class="text-gray-800 text-hover-primary">reSell</a>
+        </div>
+        <!--end::Copyright-->
+    </div>
+    <!--end::Container-->
+</div>
+<!--end::Footer-->
