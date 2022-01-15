ee5f4cd79fc791a70de0d1f2d6389efe087d6b97
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-15 23:16:34 +0700

    fix migrasi

---
+++ database/migrations/2022_01_15_151524_create_provinsis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvinsisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tm_provinsi', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_provinsi');
+            $table->string('nama_provinsi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tm_provinsi');
+    }
+}
