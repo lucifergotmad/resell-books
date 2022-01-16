cac783befbcc2bd332ea6f54353320c163c8c82f
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-16 18:11:23 +0700

    fix alamat model

--- app/Models/Alamat.php
+++ app/Models/Alamat.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Alamat extends Model
 {
     use HasFactory;
+
+    protected $table = 'tm_alamat';
+    protected $fillable = ['kode_member', 'alamat', 'kode_provinsi', 'kode_kota', 'kode_kecamatan', 'kode_pos'];
+
+    public function member()
+    {
+        return $this->hasOne(Member::class);
+    }
+
+    public function provinsi()
+    {
+        return $this->belongsTo(Provinsi::class);
+    }
+
+    public function kota()
+    {
+        return $this->belongsTo(Kota::class);
+    }
+
+    public function kecamatan()
+    {
+        return $this->belongsTo(Kecamatan::class);
+    }
 }
