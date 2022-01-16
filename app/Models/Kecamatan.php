6012291a7e18a047d7c4612e8c620da0dda0ae5f
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-16 18:00:49 +0700

    fix model kecamatan

--- app/Models/Kecamatan.php
+++ app/Models/Kecamatan.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Kecamatan extends Model
 {
     use HasFactory;
+
+    public function kota()
+    {
+        return $this->hasOne(Kota::class);
+    }
 }
