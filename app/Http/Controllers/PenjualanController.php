0024d8eb8f92a5509b3ed8937188b5f0d798b071
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-01-17 22:26:54 +0700

    fix major

--- app/Http/Controllers/PenjualanController.php
+++ app/Http/Controllers/PenjualanController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Buku;
 use App\Models\Member;
+use App\Models\Penjualan;
+use App\Models\PenjualanDetail;
+use App\Models\StockBuku;
 use Illuminate\Http\Request;
 
 class PenjualanController extends Controller
@@ -37,7 +41,42 @@ class PenjualanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'no_penjualan' => 'required',
+            'tanggal' => 'required',
+            'kode_buku' => 'required',
+            'qty' => 'required',
+            'diskon' => 'required',
+        ]);
+
+        $kode_member = (!$request->kode_member) ? "-" : $request->kode_member;
+        $buku = Buku::where('kode_buku', $request->kode_buku)->get()->first();
+        $total_berat = $request->qty * $buku->berat;
+        $diskon = (!$request->diskon) ? 0 : (($request->qty * $buku->harga_jual) * $request->diskon) / 100;
+        $total_harga = ($request->qty * $buku->harga_jual) - $diskon;
+
+        Penjualan::create([
+            'no_penjualan' => $request->no_penjualan,
+            'kode_member' => $kode_member,
+            'tanggal' => $request->tanggal,
+            'total_berat' => $total_berat,
+            'total_qty' => $request->qty,
+            'total_harga' => $total_harga,
+            'diskon' => $request->diskon,
+        ]);
+
+        PenjualanDetail::create([
+            'no_penjualan' => $request->no_penjualan,
+            'kode_buku' => $request->kode_buku,
+            'berat' => $buku->berat,
+            'qty' => $request->qty,
+            'harga_jual' => $total_harga,
+        ]);
+
+        StockBuku::where('kode_buku', $request->kode_buku)->decrement('total_berat', $total_berat);
+        StockBuku::where('kode_buku', $request->kode_buku)->decrement('total_qty', $request->qty);
+
+        return redirect()->route('penjualan.index')->with('success', 'Penjualan berhasil!');
     }
 
     /**
